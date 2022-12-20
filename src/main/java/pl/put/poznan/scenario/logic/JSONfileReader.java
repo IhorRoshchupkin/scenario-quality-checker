7189e8a7104013cb670627b168f493fd17a5c3e0
MykolaMuts <mutsmykola@gmail.com>
2022-12-20 16:14:10 +0100

    JSONReader

---
+++ src/main/java/pl/put/poznan/scenario/logic/JSONfileReader.java
@@ -0,0 +1,36 @@
+package pl.put.poznan.scenario.logic;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JSONfileReader
+{
+
+    private File getFileFromResources(String filename)
+    {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(filename);
+        if (resource == null) {
+            throw new IllegalArgumentException("File is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
+    }
+
+    public String toString(String filename)
+    {
+        String filepath = getFileFromResources(filename + ".json").getPath();
+        String content = "";
+        try {
+            content = new String ( Files.readAllBytes( Paths.get(filepath) ) );
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+}
