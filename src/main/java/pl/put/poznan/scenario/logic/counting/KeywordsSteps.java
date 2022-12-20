2c159975af69002505abe17c37630b31d882991e
AScriet <99024245+AScriet@users.noreply.github.com>
2022-12-20 18:34:40 +0100

    Counting(documentation)

--- src/main/java/pl/put/poznan/scenario/logic/counting/KeywordsSteps.java
+++ src/main/java/pl/put/poznan/scenario/logic/counting/KeywordsSteps.java
@@ -4,23 +4,39 @@ import pl.put.poznan.scenario.model.Step;
 
 import java.util.List;
 
+/**
+ * Klasa do zliczania kroków zawierających slowa kluczowe
+ *
+ */
 public class KeywordsSteps extends CountingVisitor
 {
     private int stepsNumber = 0;
 
     public int getStepsNumber() { return stepsNumber; }
 
+    /**
+     * Zlicza liczbę kroków zawierających słowa kluczowe.
+     *
+     * @param step  aktualnie odwiedzany krok
+     */
     public void count(Step step)
     {
         if(isKeyword(step))
             stepsNumber++;
     }
-
+    /**
+     * Ustawia stepsNumber na zero
+     */
     public void afterCounting()
     {
         stepsNumber = 0;
     }
 
+    /**
+     * Sprawdza czy krok zawiera slowa kluczowe
+     * @param step krók do sprawdzenia
+     * @return  true jezeli krok zawiera slowo kluczowe bądz false jezeli nie zawiera
+     */
     private static boolean isKeyword(Step step)
     {
         List<String> content = step.getContent();
