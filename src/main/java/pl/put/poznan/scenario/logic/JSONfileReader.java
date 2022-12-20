568d0751f7fb1b0ae1f1aaa33ba6c59e95869863
AScriet <99024245+AScriet@users.noreply.github.com>
2022-12-20 18:30:37 +0100

    Documentation

--- src/main/java/pl/put/poznan/scenario/logic/JSONfileReader.java
+++ src/main/java/pl/put/poznan/scenario/logic/JSONfileReader.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
+/**
+ * Klasa do otczytania pliku w formacie JSON oraz konwertowania w String
+ *
+ */
 public class JSONfileReader
 {
-
+    /**
+     * Funkcja do odczytywania pliku o wskazanym folderze
+     * @param filename  nazwa pliku
+     * @return          plik w formacie JSON
+     */
     private File getFileFromResources(String filename)
     {
         ClassLoader classLoader = getClass().getClassLoader();
@@ -21,12 +30,18 @@ public class JSONfileReader
         }
     }
 
+    /**
+     * Funkcja do konwertowania JSON w String
+     * @param filename nazwa pliku
+     * @return         scenariusz w formacie String
+     */
     public String toString(String filename)
     {
         String filepath = getFileFromResources(filename + ".json").getPath();
         String content = "";
         try {
-            content = new String ( Files.readAllBytes( Paths.get(filepath) ) );
+            content = new String ( Files.readAllBytes( Paths.get(filepath.replaceAll("%20"," "))));
+
         }
         catch (IOException e) {
             e.printStackTrace();
